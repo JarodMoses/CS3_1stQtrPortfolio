5093287aa8d05cd1976b682507215d39657a70b1
JarodMoses <b2029jmacajustin@pshs.edu.ph>
2025-08-05 08:45:23 +0800

    hehe

--- public/script.js
+++ public/script.js
@@ -1,1 +1,7 @@
-document.write ("heyyyy :)" )
+let age = 14;
+let maxAge = 101;
+let numPerDay = 2;
+
+totalRequired = (numPerDay * 365) * (maxAge - age)
+
+document.write("You will need " + totalRequired + " wafers to last you until the ripe old age of " + maxAge)
