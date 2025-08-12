e9f70acb03e07007dbfd12b86bb090a32a9a176d
JarodMoses <b2029jmacajustin@pshs.edu.ph>
2025-08-12 08:23:34 +0800

    Speed Test 1

--- public/script.js
+++ public/script.js
@@ -1,7 +1,13 @@
-let age = 14;
-let maxAge = 101;
-let numPerDay = 2;
+let age = window.prompt("Enter your age:");
+let maxAge = window.prompt("Enter the age you want to live up to:");
+let numPerDay = window.prompt("Enter how many wafers you eat per day:");
 
 totalRequired = (numPerDay * 365) * (maxAge - age)
 
-document.write("You will need " + totalRequired + " wafers to last you until the ripe old age of " + maxAge)
+
+if (confirm(`Are the values you inputted really true?:`)) {
+    document.write("You will need " + totalRequired + " wafers to last you until the ripe old age of " + maxAge)
+    alert(`You will need ${totalRequired} wafers to last you until the ripe old age of ${maxAge}`) }
+else {
+document.write("Run the code again:)")
+}
